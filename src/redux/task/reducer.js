7d145071def63f2742d691c123af7ce258633bc9
Daniel Silva <165176862+Daniel13s@users.noreply.github.com>
2025-08-07 07:58:12 -0300

    Update reducer.js

--- src/redux/task/reducer.js
+++ src/redux/task/reducer.js
@@ -1,7 +1,7 @@
 import TaskActionTypes from "./action-types";
 
 const initialState = {
-  tasks: JSON.parse(localStorage.getItem("tasks")) || [],
+  tasks: localStorage.getItem("tasks") || [],
 };
 
 const taskReducer = (state = initialState, action) => {
