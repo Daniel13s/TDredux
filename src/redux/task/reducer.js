d5f5e087360e4fefa4152ae53b9da4adfc58570c
Daniel Silva <165176862+Daniel13s@users.noreply.github.com>
2025-08-07 23:21:47 -0300

    Update reducer.js

--- src/redux/task/reducer.js
+++ src/redux/task/reducer.js
@@ -8,7 +8,8 @@ const taskReducer = (state = initialState, action) => {
   switch (action.type) {
     case TaskActionTypes.CREATE:
       const newTask = action.payload;
-      const lista = [state.tasks, newTask];
+      const tasks = JSON.parse(localStorage.getItem("tasks"))
+      const lista = [tasks, newTask];
       localStorage.setItem("tasks", JSON.stringify(lista));
       return { ...state, tasks: lista };
 
