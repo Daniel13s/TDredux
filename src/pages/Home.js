a9497613700fd9f341975611e6209d18aad04b13
Daniel Silva <165176862+Daniel13s@users.noreply.github.com>
2025-08-07 23:14:21 -0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -66,7 +66,7 @@ function Home() {
         })
     }
     function tasksCheck() {
-        tasks = localStorage.getItem("tasks")
+        tasks = JSON.parse(localStorage.getItem("tasks"))
         const tasksCheck = tasks.filter(task => task.isComplete !== false)
         dispatch({
             type: TaskActionTypes.FILTERCHECK,
@@ -75,7 +75,7 @@ function Home() {
     }
 
     function tasksPendentes() {
-        tasks = localStorage.getItem("tasks")
+        tasks = JSON.parse(localStorage.getItem("tasks"))
         const tasksPen = tasks.filter(task => task.isComplete !== true)
         dispatch({
             type: TaskActionTypes.FILTERPEN,
@@ -84,7 +84,7 @@ function Home() {
     }
 
     function allTasks() {
-        const allTasks = localStorage.getItem('tasks')
+        const allTasks = JSON.parse(localStorage.getItem("tasks"))
         dispatch({
             type: TaskActionTypes.ALLTASKS,
             payload: allTasks
